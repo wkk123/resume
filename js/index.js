38de1b5d314a63d09b528e354a6c9fcf35a00e94
WangKangKang <1754393348@qq.com>
2016-08-06 10:44:54 +0800

    chushihua

---
+++ js/index.js
@@ -0,0 +1,32 @@
+$(function(){
+	$('.container-fluid .moto-nav .element .color1').on('mouseover',function(){
+        $('.container-fluid .moto-nav .element .color1').closest('div').addClass('color1')
+	})
+	$('.container-fluid .moto-nav .element .color2').on('mouseover',function(){
+        $('.container-fluid .moto-nav .element .color2').closest('div').addClass('color2')
+	})
+	$('.container-fluid .moto-nav .element .color3').on('mouseover',function(){
+        $('.container-fluid .moto-nav .element .color3').closest('div').addClass('color3')
+	})
+	$('.container-fluid .moto-nav .element .color4').on('mouseover',function(){
+        $('.container-fluid .moto-nav .element .color4').closest('div').addClass('color4')
+	})
+	$('.container-fluid .moto-nav .element .color5').on('mouseover',function(){
+        $('.container-fluid .moto-nav .element .color5').closest('div').addClass('color5')
+	})
+	$('.container-fluid .moto-nav .element .color1').on('mouseout',function(){
+        $('.container-fluid .moto-nav .element .color1').closest('div').removeClass('color1')
+	})
+	$('.container-fluid .moto-nav .element .color2').on('mouseout',function(){
+        $('.container-fluid .moto-nav .element .color2').closest('div').removeClass('color2')
+	})
+	$('.container-fluid .moto-nav .element .color3').on('mouseout',function(){
+        $('.container-fluid .moto-nav .element .color3').closest('div').removeClass('color3')
+	})
+	$('.container-fluid .moto-nav .element .color4').on('mouseout',function(){
+        $('.container-fluid .moto-nav .element .color4').closest('div').removeClass('color4')
+	})
+	$('.container-fluid .moto-nav .element .color5').on('mouseout',function(){
+        $('.container-fluid .moto-nav .element .color5').closest('div').removeClass('color5')
+	})
+})
